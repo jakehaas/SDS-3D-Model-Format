405cb68344840074c79ff711dde210b85d010aaa
Jake Haas <jake@jakehaas.com>
2014-12-17 21:04:11 -0500

    Inital Commit

---
+++ SDS Scripts/Unity/SDSExport.js
@@ -0,0 +1,120 @@
+// SDS-3D-Model-Format
+// SDSExport.js
+//
+// Copyright (c) 2013, 2014. Jake Haas. All Right Reserved, http://jakehaas.com/
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+
+import System.IO;
+import System.Text;
+import System.Diagnostics; 
+
+@MenuItem ("Assets/Export SDS %#e")
+static function ExportToString()
+{
+	// Check selection
+	if (Selection.activeGameObject == null)
+	{
+		EditorUtility.DisplayDialog("Export Error", "Please select an object to export.", "OK");
+		return;
+	}
+	
+	if (Selection.activeGameObject.GetComponent("MeshFilter") == null)
+	{
+		EditorUtility.DisplayDialog("Export Error", "You can only export objects with geometry.", "OK");
+		return;
+	}
+	
+	// Open dialog to file output
+	savePath = EditorUtility.SaveFilePanel("SDS Export", "/usr/local/Desktop/", Selection.activeGameObject.name + ".sds", "sds");
+	
+	// Open output stream
+	var sw : StreamWriter = new StreamWriter(savePath);
+	
+		// Write header
+	var sdsString : String;
+	sw.Write("// SDS format by Jake Haas\n// Exported with Unity " + Application.unityVersion + "\n\n");
+
+	// Iterate through selection
+	for (gameObject in Selection.gameObjects)
+	{
+		// Setup mesh info
+		var mesh = gameObject.GetComponent("MeshFilter").sharedMesh;
+		var trans:Transform = gameObject.transform;
+
+		// Write object name
+		sw.Write("StartObject [" + mesh.name + "] {\n\n");
+
+		// Write transformation
+		sw.Write("Translation [" + trans.position.x + "," + trans.position.y + "," + trans.position.z + "]\n");
+		sw.Write("Rotation [" + trans.eulerAngles.x + "," + trans.eulerAngles.y + "," + trans.eulerAngles.z + "]\n");
+		sw.Write("Scale [" + trans.localScale.x + "," + trans.localScale.y + "," + trans.localScale.z + "]\n\n");
+			
+		// Write number of vertices, triangles, normals and uv
+		sw.Write("numVertices [" + mesh.vertexCount + "]\n");
+		sw.Write("numTriangles [" + mesh.triangles.Length / 3 + "]\n");
+		sw.Write("numNormals [" + mesh.normals.Length + "]\n");
+		sw.Write("numUVs [" + mesh.uv.Length + "]\n\n");
+
+		// Vertex positions
+		sw.Write("Vertices {\n");
+			
+		// Write vertex coordinates
+		for (i = 0; i < mesh.vertices.Length; i++)
+		{
+			sw.Write(i + " [" + mesh.vertices[i].x + "," + mesh.vertices[i].y + "," + mesh.vertices[i].z + "]\n");
+		}
+		
+		sw.Write("}\n\n");
+			
+			
+		// Triangulation description
+		sw.Write("Triangles {\n");
+			
+		// Write triangle index pointers
+		for (i = 0; i < mesh.triangles.Length; i += 3)
+		{
+			sw.Write(i / 3 + " [");
+			sw.Write(mesh.triangles[i] + "," + mesh.triangles[i + 1] + "," + mesh.triangles[i + 2] + "]\n");
+		}
+		
+		sw.Write("}\n\n");
+
+
+		// Normal vectors
+		sw.Write("Normals {\n");
+			
+		// Write normal vector coordinates
+		for (i = 0; i < mesh.normals.Length; i++)
+		{
+			sw.Write(i + " [");
+			sw.Write(mesh.normals[i].x + "," + mesh.normals[i].y + "," + mesh.normals[i].z + "]\n");
+		}
+		
+		sw.Write("}\n\n");
+
+
+		// UVs
+		sw.Write("UVs {\n");
+			
+		// Write UV values
+		for (i = 0; i < mesh.uv.Length; i++)
+		{
+			sw.Write(i + " [");
+			sw.Write(mesh.uv[i].x + "," + mesh.uv[i].y + "]\n");
+		}
+		
+		sw.Write("}\n\n");
+
+		// Close object block
+		sw.Write("} [EndObject]\n\n");
+	}
+        
+	// Finalize and close
+	sw.Close();
+
+	print(mesh.name + " exported successfully!");
+}
---
+++ SDS Scripts/Unity/SDSImport.js
@@ -0,0 +1,223 @@
+// SDS-3D-Model-Format
+// SDSImport.js
+//
+// Copyright (c) 2013, 2014. Jake Haas. All Right Reserved, http://jakehaas.com/
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+
+import System.Text.RegularExpressions;
+import System;
+import System.IO;
+
+@MenuItem ("Assets/Import SDS %i")
+static function ImportSDS()
+{
+	// Open dialog for file input
+	filepath = EditorUtility.OpenFilePanel("Import SDS", "","sds");
+	
+    if (File.Exists(filepath))
+	{	
+        // Open file
+		var sdsReader : StreamReader = new StreamReader(filepath);
+        var sdsLine : String;
+		sdsLine = sdsReader.ReadLine();
+    
+		var objectName : String;
+		var translation : Vector3;
+		var rotation : Vector3;
+		var scale : Vector3;
+		var numVertices : int;
+		var numTriangles : int;
+		var numNormals : int;
+		var numUVs : int;
+		var vtx;
+		var tri;
+		var norm;
+		var uv;
+		
+		// Scan each line
+		while(sdsLine!=null)
+		{
+			// Get object name
+			if (Regex.Match(sdsLine,"StartObject").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split("]"[0]);
+				objectName = subTemp[0];
+			}
+			// Get translation
+			else if (Regex.Match(sdsLine,"Translation").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split(","[0]);
+
+				translation.x = float.Parse(subTemp[0]);
+				translation.y = float.Parse(subTemp[1]);
+				translation.z = float.Parse(subTemp[2].Substring(0, subTemp[2].Length - 1));
+			}
+			// Get rotation
+			else if (Regex.Match(sdsLine,"Rotation").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split(","[0]);
+
+				rotation.x = float.Parse(subTemp[0]);
+				rotation.y = float.Parse(subTemp[1]);
+				rotation.z = float.Parse(subTemp[2].Substring(0, subTemp[2].Length - 1));
+			}
+			// Get scale
+			else if (Regex.Match(sdsLine,"Scale").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split(","[0]);
+
+				scale.x = float.Parse(subTemp[0]);
+				scale.y = float.Parse(subTemp[1]);
+				scale.z = float.Parse(subTemp[2].Substring(0, subTemp[2].Length - 1));
+			}
+			// Get number of vertices
+			else if (Regex.Match(sdsLine,"numVertices").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split("]"[0]);
+
+				numVertices = int.Parse(subTemp[0]);
+			}
+			// Get number of triangles
+			else if (Regex.Match(sdsLine,"numTriangles").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split("]"[0]);
+
+				numTriangles = int.Parse(subTemp[0]);
+			}
+			// Get number of normals
+			else if (Regex.Match(sdsLine,"numNormals").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split("]"[0]);
+
+				numNormals = int.Parse(subTemp[0]);
+			}
+			// Get number of UVs
+			else if (Regex.Match(sdsLine,"numUVs").Success)
+			{
+				temp = sdsLine.Split("["[0]);
+				subTemp = temp[1].Split("]"[0]);
+
+				numUVs = int.Parse(subTemp[0]);
+			}
+			// Get vertices
+			else if (Regex.Match(sdsLine,"Vertices").Success)
+			{
+				vtx = new Vector3[numVertices];
+				i = 0;
+				sdsLine = sdsReader.ReadLine();
+				
+				while (!Regex.Match(sdsLine,"}").Success)
+				{
+					temp = sdsLine.Split("["[0]);
+					subTemp = temp[1].Split(","[0]);
+
+					vtx[i++] = new Vector3(float.Parse(subTemp[0]), 
+											float.Parse(subTemp[1]), 
+											float.Parse(subTemp[2].Substring(0, subTemp[2].Length - 1)));
+											
+					sdsLine = sdsReader.ReadLine();
+				}
+			}
+			// Get triangles
+			else if (Regex.Match(sdsLine,"Triangles").Success)
+			{
+				tri = new int[numTriangles * 3];
+				i = 0;
+				sdsLine = sdsReader.ReadLine();
+				
+				while (!Regex.Match(sdsLine,"}").Success)
+				{
+					temp = sdsLine.Split("["[0]);
+					subTemp = temp[1].Split(","[0]);
+
+					tri[i++] = int.Parse(subTemp[0]);
+					tri[i++] = int.Parse(subTemp[1]);
+					tri[i++] = int.Parse(subTemp[2].Substring(0, subTemp[2].Length - 1));
+					
+					sdsLine = sdsReader.ReadLine();
+					print (tri[i - 1] + "   " + i);
+				}
+			}
+			// Get normals
+			else if (Regex.Match(sdsLine,"Normals").Success)
+			{
+				norm = new Vector3[numNormals];
+				i = 0;
+				sdsLine = sdsReader.ReadLine();
+				
+				while (!Regex.Match(sdsLine,"}").Success)
+				{
+					temp = sdsLine.Split("["[0]);
+					subTemp = temp[1].Split(","[0]);
+
+					norm[i++] = new Vector3(float.Parse(subTemp[0]), float.Parse(subTemp[1]), float.Parse(subTemp[2].Substring(0, subTemp[2].Length - 1)));
+					sdsLine = sdsReader.ReadLine();
+				}
+			}
+			// Get UVs
+			else if (Regex.Match(sdsLine,"UVs").Success)
+			{
+				uv = new Vector2[numUVs];
+				i = 0;
+				sdsLine = sdsReader.ReadLine();
+				
+				while (!Regex.Match(sdsLine,"}").Success)
+				{
+					temp = sdsLine.Split("["[0]);
+					subTemp = temp[1].Split(","[0]);
+
+					uv[i++] = new Vector2(float.Parse(subTemp[0]), float.Parse(subTemp[1].Substring(0, subTemp[1].Length - 1)));
+					sdsLine = sdsReader.ReadLine();
+				}
+			}
+			// Create object
+			else if (Regex.Match(sdsLine,"EndObject").Success)
+			{
+				var newObj : GameObject = new GameObject(objectName);
+				newObj.AddComponent(MeshFilter);
+				newObj.AddComponent(MeshRenderer);
+	
+				var mesh = new Mesh ();
+				newObj.GetComponent(MeshFilter).mesh = mesh;
+	
+				// Assign mesh data
+				mesh.vertices = vtx;
+				mesh.triangles = tri;
+				mesh.normals = norm;
+				mesh.uv = uv;
+				
+				// Apply transformation
+				newObj.transform.position = translation;
+				newObj.transform.eulerAngles = rotation;
+				newObj.transform.localScale = scale;
+				
+				// Save to Prefab
+				newObj.active = false;
+				EditorUtility.ReplacePrefab (newObj, EditorUtility.CreateEmptyPrefab("Assets/" + objectName + ".prefab"));
+				DestroyImmediate (newObj);
+				AssetDatabase.Refresh();
+				
+				print(filepath + " imported successfully!");
+			}			
+			
+            sdsLine = sdsReader.ReadLine();
+        }
+		
+    }
+	else
+	{
+        EditorUtility.DisplayDialog("Import Error", "SDS file not found!", "Cancel");
+        return;
+    }
+}
